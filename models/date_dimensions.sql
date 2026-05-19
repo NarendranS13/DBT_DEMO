13fe4fa90ab1e7ce9c106cb29a90a47096b80b18
Narendran Sundhar <nren115@gmail.com>
2026-05-19 12:53:20 +0000

    daily_weather model created

--- models/date_dimensions.sql
+++ models/date_dimensions.sql
@@ -4,19 +4,9 @@ SELECT
 to_timestamp(started_at) AS Started_at,
 DATE(to_timestamp(started_at)) AS Date_started_at,
 HOUR(to_timestamp(started_at)) AS Hour_started_at,
-CASE WHEN DAYNAME(to_timestamp(started_at)) in ('Sat','Sun')
-THEN 'WEEKEND'
-ELSE 'BUSINESSDAY'
-END AS DAY_TYPE,
+{{day_type('STARTED_AT')}} AS DAY_TYPE,
+{{get_season('STARTED_AT')}} AS STATION_OF_YEAR
 
-CASE WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (12,1,2)
-THEN 'WINTER'
-WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (3,4,5)
-THEN 'SPRING'
-WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (6,7,8)
-THEN 'SUMMER'
-ELSE 'AUTUMN'
-END AS STATION_OF_YEAR
 FROM 
 {{ source('demo', 'BIKE') }}
 WHERE STARTED_AT != 'started_at'
